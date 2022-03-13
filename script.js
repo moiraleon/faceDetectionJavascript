510f8e3a8625c3606dd46a5835c6aa631d3f223e
Moira Leon <moira.leon2@gmail.com>
2022-03-12 23:35:16 -0700

    properly sizing detection box elements for video and clearing and allowing for resizing each time

--- script.js
+++ script.js
@@ -20,10 +20,13 @@ video.addEventListener('play',()=>{
     const canvas = faceapi.createCanvasFromMedia(video)
     document.body.append(canvas)
     const displaySize = {width: video.width, height: video.height}
+    faceapi.matchDimensions(canvas, displaySize)
     console.log('Checking for video load')
     setInterval(async()=> {
         const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
         console.log(detections)
         const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0,0, canvas.width, canvas.height) 
+        faceapi.draw.drawDetections(canvas, resizedDetections)
     },100)
 })
